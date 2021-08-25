ee8fdee4f4ed284a8b2a9987f68c9b843d322e23
Ram Varra <ramvarra@hotmail.co>
2021-08-24 21:06:49 -0700

    with

---
+++ src/main/java/com/ramvarra/app/App.java
@@ -0,0 +1,17 @@
+package com.ramvarra.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Num Args: " + args.length);
+        for (String s: args) {
+            System.out.println("Arg = " + s);
+        }
+        System.out.println("Hello World!");
+    }
+}
